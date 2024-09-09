ddf11d12c9459953eacddacdc972b12a0798750e
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-08 22:34:22 -0300

    check cuil/cuit / send data on login

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -9,6 +9,13 @@ const crearEmpresa= async(req,res = response) =>{
     const {mail,pass}=req.body;
 
     try {
+        const flagcc=checkCuilCuit(req.body.cuil_cuit);
+        if(!flagcc){
+            return res.status(400).json({
+                ok:false,
+                msg:'Cuil/Cuit invalido'
+            });
+        }
         const existeEmail= await Usuario.findOne({mail});
         if(existeEmail){
             return res.status(400).json({
@@ -90,15 +97,17 @@ const login=async(req,res=response)=>{
             })
         }
 
+        const token= await generarJWT(cuentaEncontrada.id);
         if(!cuentaEncontrada.validado || !cuentaEncontrada.habilitado){
             res.json({
                 ok:false,
                 validado: cuentaEncontrada.validado,
                 habilitado: cuentaEncontrada.habilitado,
-                mail: cuentaEncontrada.mail
+                mail: cuentaEncontrada.mail,
+                token,
+                user: cuentaEncontrada.nombre_comercial ? cuentaEncontrada.nombre_comercial : nombre_apellido
             })
         }else{
-            const token= await generarJWT(cuentaEncontrada.id);
             res.json({
                 ok:true,
                 token,
@@ -112,6 +121,28 @@ const login=async(req,res=response)=>{
             msg:'error login'
         });
     }
+}
+
+const checkCuilCuit=(cc)=>{		
+    var rv = false;
+    var resultado = 0;
+    var cc_nro = cc.replace("-", "");
+    var codes = "6789456789";
+    var verificador = parseInt(cc_nro[cc_nro.length-1]);
+    var x = 0;
+    
+    while (x < 10){
+        var digitoValidador = parseInt(codes.substring(x, x+1));
+        if (isNaN(digitoValidador)) digitoValidador = 0;
+        var digito = parseInt(cc_nro.substring(x, x+1));
+        if (isNaN(digito)) digito = 0;
+        var digitoValidacion = digitoValidador * digito;
+        resultado += digitoValidacion;
+        x++;
+    }
+    resultado = resultado % 11;
+    rv = (resultado == verificador);
+    return rv;
 }
 
 module.exports={crearEmpresa, login}
