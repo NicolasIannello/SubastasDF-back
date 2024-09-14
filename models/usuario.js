adafbd0b92ac150d205abad2c75fb2724df3ce25
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-14 15:01:26 -0300

    order

--- models/usuario.js
+++ models/usuario.js
@@ -2,21 +2,21 @@ const { Schema, model } = require('mongoose');
 
 const UsuarioSchema = Schema({
     nombre_apellido: { type: String, required: true },
-    cuil_cuit: { type: String, required: true },
-    telefono: { type: String, required: true },
-    actividad: { type: String, required: true },
-    como_encontro: { type: String, required: true },
     mail: { type: String, required: true },
+    telefono: { type: String, required: true },
+    cuil_cuit: { type: String, required: true },
+    habilitado: { type: Boolean, required: true },
     pass: { type: String },
+    actividad: { type: String, required: true },
     pais: { type: String, required: true },
     provincia: { type: String, required: true },
     ciudad: { type: String, required: true },
     postal: { type: String, required: true },
     domicilio: { type: String, required: true },
-    habilitado: { type: Boolean, required: true },
+    tipo: { type: String, required: true },
     ultima_conexion: { type: String, required: true },
     validado: { type: Boolean, required: true },
-    tipo: { type: String, required: true },
+    como_encontro: { type: String, required: true },
 });
 
 UsuarioSchema.method('toJSON', function() {
