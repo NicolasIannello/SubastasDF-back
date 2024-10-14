5af6fdb26d520e7e092531ce62f1cf7562f642d0
NicolasIannello <nicolas.iannello99@gmail.com>
2024-10-13 21:13:16 -0300

    hora inicio/cierre - segundos - mostar precio/ofertas/ganadarores - grupo

--- models/evento.js
+++ models/evento.js
@@ -6,12 +6,20 @@ const EventoSchema = Schema({
     categoria: { type: String, required: true },
     fecha_inicio: { type: String, required: true },
     fecha_cierre: { type: String, required: true },
+    hora_inicio: { type: String, required: true },
+    hora_cierre: { type: String, required: true },
+    segundos_cierre: { type: Number, required: true },
     modalidad: { type: String, required: true },
     publicar_cierre: { type: Boolean, require:true },
     home: { type: Boolean, required: true },
     eventos: { type: Boolean, required: true },
     inicio_automatico: { type: Boolean, required: true },
     visitas: { type: Number, required: true },
+    mostrar_precio: { type: Boolean, required: true },
+    mostrar_ganadores: { type: Boolean, required: true },
+    mostrar_ofertas: { type: Boolean, required: true },
+    grupo: { type: String, required: true },
+    activo: { type: Boolean, required: true },
 });
 
 EventoSchema.method('toJSON', function() {
