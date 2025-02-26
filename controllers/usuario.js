c495f189e79c066ba0f674219a7a219d91a90eca
NicolasIannello <nicolas.iannello99@gmail.com>
2025-02-26 16:00:24 -0300

    get adjudicados

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -358,67 +358,33 @@ const getDatos= async(req,res=response)=>{
 
 const getAdjudicados= async(req,res=response)=>{
     const userDB = await Usuario.findById(req.uid);
-    const lotesDB = await Lote.find({ganador:userDB.mail})
-    let adjudicadosDB = [];
-
-    if(!lotesDB){
-    adjudicadosDB=await EventoLote.aggregate([
-        { "$match": { uuid_lote:lotesDB[0].uuid } },
+    const adjudicadosDB = await Lote.aggregate([
+        { "$match": { ganador:userDB.mail } },
         { $lookup: {
-            from: "eventos",
-            localField: "uuid_evento",
-            foreignField: "uuid",
-            as: "evento"
-        } },
-        {$unwind: { path: "$evento", preserveNullAndEmptyArrays: true }},
+            from: "eventolotes",
+            localField: "uuid",
+            foreignField: "uuid_lote",
+            as: "evlot",
+            "pipeline": [ 
+                { $lookup: {
+                    from: "eventos",
+                    localField: "uuid_evento",
+                    foreignField: "uuid",
+                    as: "evento"
+                } },
+                {$unwind: { path: "$evento", preserveNullAndEmptyArrays: true }},
+                { $project: {
+                    __v: 0,"evento.__v": 0,"evento._id": 0,"evento.categoria": 0,"evento.estado": 0,"evento.eventos": 0,"evento.fecha_cierre": 0,"evento.fecha_inicio": 0,
+                    "evento.grupo": 0,"evento.home": 0,"evento.hora_cierre": 0,"evento.hora_inicio": 0,"evento.inicio_automatico": 0,"evento.modalidad": 0,
+                    "evento.mostrar_ganadores": 0,"evento.mostrar_ofertas": 0,"evento.mostrar_precio": 0,"evento.publicar_cierre": 0,"evento.segundos_cierre": 0,"evento.uuid": 0,
+                    "evento.visitas": 0,
+                } },        
+            ],
+        }},
         { $project: {
-            __v: 0,
-            "evento.__v": 0,
-            "evento._id": 0,
-            "evento.categoria": 0,
-            "evento.estado": 0,
-            "evento.eventos": 0,
-            "evento.fecha_cierre": 0,
-            "evento.fecha_inicio": 0,
-            "evento.grupo": 0,
-            "evento.home": 0,
-            "evento.hora_cierre": 0,
-            "evento.hora_inicio": 0,
-            "evento.inicio_automatico": 0,
-            "evento.modalidad": 0,
-            "evento.mostrar_ganadores": 0,
-            "evento.mostrar_ofertas": 0,
-            "evento.mostrar_precio": 0,
-            "evento.publicar_cierre": 0,
-            "evento.segundos_cierre": 0,
-            "evento.uuid": 0,
-            "evento.visitas": 0,
-        } },
-        { $lookup: {
-            from: "lotes",
-            localField: "uuid_lote",
-            foreignField: "uuid",
-            as: "lote"
-        } },
-        {$unwind: { path: "$lote", preserveNullAndEmptyArrays: true }},
-        { $project: {
-            __v: 0,
-            "lote.__v": 0,
-            "lote._id": 0,
-            "lote.aclaracion": 0,
-            "lote.base_salida": 0,
-            "lote.descripcion": 0,
-            "lote.disponible": 0,
-            "lote.ganador": 0,
-            "lote.incremento": 0,
-            "lote.moneda": 0,
-            "lote.precio_base": 0,
-            "lote.precio_salida": 0,
-            "lote.terminos_condiciones": 0,
-            "lote.uuid": 0,
+            __v: 0,"evlot.__v": 0,"evlot._id": 0,"evlot.uuid_lote": 0,
         } },
     ]).collation({locale: 'en'});
-    }
 
     res.json({
         ok:true,
